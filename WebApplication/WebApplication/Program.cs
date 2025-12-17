501a69016050de470873b9c4444956a92f9974ab
FloWittmann <florian.wittmann@peakavenue.com>
2025-12-17 13:17:58 +0100

    chnage summaries

--- WebApplication/WebApplication/Program.cs
+++ WebApplication/WebApplication/Program.cs
@@ -16,7 +16,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezing", "Cold", "Mild", "Warm", "Hot"
 };
 
 app.MapGet("/weatherforecast", () =>
